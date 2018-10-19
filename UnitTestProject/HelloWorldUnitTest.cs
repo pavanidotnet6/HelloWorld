2ccbb064870571861c2c49f9997051962c219b37
pavanidotnet6 <44303013+pavanidotnet6@users.noreply.github.com>
2018-10-19 14:42:31 -0500

    Add files via upload

---
+++ UnitTestProject/HelloWorldUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shared.UnitOfWork;
+using HW.UnitOfWork;
+using Shared.Service;
+using Service.HW;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class HelloWorldUnitTest
+    {
+        /// <summary>
+        /// This function would make call to console app n return output string
+        /// </summary>
+        [TestMethod]
+        public void HelloWorldTestMethod()
+        {
+            string inputText = "input";
+            string outPutText = "Hello World!";
+            var result = ConsoleApplication.Program.HelloWorldFunction(inputText);
+            Assert.AreEqual(result, outPutText);
+        }
+    }
+}
