da3ea670625b1062faa13c001983aa36fc48f265
pavanidotnet6 <44303013+pavanidotnet6@users.noreply.github.com>
2018-10-19 14:40:42 -0500

    Add files via upload

---
+++ Service.HW/HW.UnitOfWork/HelloWorldUnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using IAAI.ASAP.Repository.Data;
+using Shared.UnitOfWork;
+using System.Data.Entity;
+using WebApplication1;
+
+namespace HW.UnitOfWork
+{
+    public class HelloWorldUnitOfWork : IHelloWorldUnitOfWork
+    {
+        /// <summary>
+        /// Returns data from Database
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public string GetString(string text)
+        {
+            var result = string.Empty;
+            using (var entities = ASAPDataFunctions.Functions)
+            {
+                result = (from h in entities.Sample_Table where h.Sample_Column == text select h.Sample_Description).FirstOrDefault();
+            }
+            return result;
+        }
+    }
+}
---
+++ Service.HW/HelloWorldService.cs
@@ -0,0 +1,27 @@
+﻿using Shared.Service;
+using Shared.UnitOfWork;
+
+namespace Service.HW
+{
+    public class HelloWorldService : IHelloWorldService
+    {
+        private IHelloWorldUnitOfWork _uow;
+        public HelloWorldService(IHelloWorldUnitOfWork hwUOW)
+        {
+            _uow = hwUOW;
+        }
+
+        /// <summary>
+        /// To read text from databse thru repository
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public string GetTextFromUnitOfWork(string text)
+        {
+            string result = string.Empty;
+            result = _uow.GetString(text);
+            return result;
+        }
+
+    }
+}
