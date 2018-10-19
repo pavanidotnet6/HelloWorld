773441dba4afeabac37188ac78bdcd6d6a7ccb65
pavanidotnet6 <44303013+pavanidotnet6@users.noreply.github.com>
2018-10-19 14:48:31 -0500

    Add files via upload

---
+++ WebApp/Controllers/ValuesController.cs
@@ -0,0 +1,68 @@
+﻿using Shared.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    //[Authorize]
+    public class ValuesController : ApiController
+    {
+        private IHelloWorldService _hellowrorldService;
+
+        [System.Web.Http.HttpGet]
+        public HttpResponseMessage Index()
+        {
+            HttpResponseMessage temp = new HttpResponseMessage();
+            return temp;
+        }
+
+
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+        /// <summary>
+        /// API call 
+        /// </summary>
+        /// <param name="ci"></param>
+        /// <returns></returns>
+        [System.Web.Http.Route("api/HelloWorld")]
+        public HttpResponseMessage HelloWorld(string ci)
+        {
+
+            var outputText = _hellowrorldService.GetTextFromUnitOfWork(ci);
+
+            var resp = new HttpResponseMessage(HttpStatusCode.OK);
+
+            resp.Content = new StringContent(ci, System.Text.Encoding.UTF8, "text/plain");
+
+            return resp;
+        }
+    }
+}
