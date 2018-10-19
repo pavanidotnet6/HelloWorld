23e459e769b5290763bebf7078cf9d18c20bb876
pavanidotnet6 <44303013+pavanidotnet6@users.noreply.github.com>
2018-10-19 14:38:51 -0500

    Add files via upload

---
+++ Repository.Data/DataFunctions.cs
@@ -0,0 +1,23 @@
+﻿using Repository.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1;
+
+namespace IAAI.ASAP.Repository.Data
+{
+    public class ASAPDataFunctions
+    {
+        public static ASAPSQLEntities Functions
+        {
+            get
+            {
+                return new ASAPSQLEntities();
+            }
+        }
+
+
+    }
+}
